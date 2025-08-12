1519235cc4a4aa3c24cbe4730eef8726f6abcb0d
dwrigley1 <dwrigley1@ivytech.edu>
2025-08-12 06:45:33 -0400

    express() added

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,9 @@
-const express = require("express")
+const express = require("express") // allows us to make a backend server
+const app = express() // activate this app variable to be an express server
+const router = express.Router()
+
+app.listen(3000,function(){ // start the web server.. app.listen(portnumber, function)
+    console.log("Listening on port 3000")
+})
+
+
