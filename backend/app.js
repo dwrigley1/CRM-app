c04050d435babff1698e0217f0af1bfd4d11c5d0
dwrigley1 <dwrigley1@ivytech.edu>
2025-08-15 06:35:16 -0400

    added api functionality /api/clients

--- backend/app.js
+++ backend/app.js
@@ -7,3 +7,16 @@ app.listen(3000,function(){ // start the web server.. app.listen(portnumber, fun
 })
 
 
+router.get("/clients", function(req,res){
+    const client = {
+        name: "John Doe",
+        address: "123 Main St.",
+        account_number: "4481234",
+        system_type: "intrusion"
+    }
+    res.json(client)
+})
+
+// requests that use an API usually start with /api
+app.use("/api", router)
+//app.listen(3000)
