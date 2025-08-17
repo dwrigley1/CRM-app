3b3da84b70daaf444165bcef5bd605200f194fb4
dwrigley1 <dwrigley1@ivytech.edu>
2025-08-17 10:21:46 -0400

    removed .txt file

--- frontend/script.js
+++ frontend/script.js
@@ -3,14 +3,14 @@
 
 addEventListener("DOMContentLoaded", async function() {
     
-    const response = await fetch("https://crm-app-zfvz.onrender.com")
+    const response = await fetch("https://crm-app-zfvz.onrender.com/api/clients/")
 
     const clients = await response.json();
 
     let html = ""; // blank screen
     for (let client of clients) {
         let clientID = client._id
-        html+=`<li>${clientID}`
+        html+=`<li>${clientID}</li>`
     }
 
     document.querySelector("#list_of_clients").innerHTML = html;
