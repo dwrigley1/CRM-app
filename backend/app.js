1ab0f214a0327a865c7fe0faaaa19351463ca100
dwrigley1 <dwrigley1@ivytech.edu>
2025-08-19 06:52:56 -0400

    added PUT, POST, DELETE

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,8 @@ require("./database"); // MongoDB should see the process now
    // console.log("Listening on port 3000")
 //})
 
+// get client
+// existing function has passed testing 
 router.get("/clients", async(req,res) =>{
   try{
     const clients = await Client.find({}) 
@@ -27,6 +29,68 @@ router.get("/clients", async(req,res) =>{
     console.log(err)
   }
 })
+
+// added 8/19
+// get client by id
+router.get("/clients/:id", async (req, res) => {
+  try {
+    const client = await Song.findById(req.params.id);
+    if (!client) {
+      return res.status(404).send("Client not found");
+    }
+    res.send(client);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server error");
+  }
+});
+
+// added 8/19
+// add new client in db
+router.post("/clients", async(req,res)=>{
+  try{
+    const client = await new Song(req.body)
+    await client.save()
+    res.status(201).json(client)
+    console.log(client)
+  }
+  catch(err){
+    res.status(400).send(err)
+  }
+})
+
+// added 8/19
+// edit existing client in db
+router.put("/clients/:id", async (req, res) => {
+  try {
+    const client = req.body;
+    await Client.updateOne({ _id: req.params.id }, client);
+    console.log("Updated:", client);
+    res.sendStatus(204); // 204 = No Content, meaning success
+  } catch (err) {
+    console.error("Update error:", err);
+    res.status(400).send(err);
+  }
+});
+
+// added 8/19
+// remove client from db
+router.delete("/clients/:id", async (req, res) => {
+  console.log("DELETE request for ID:", req.params.id);
+
+  try {
+    const result = await Client.deleteOne({ _id: req.params.id });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Client not found" });
+    }
+
+    res.status(200).json({ message: "Client deleted", id: req.params.id });
+  } catch (err) {
+    console.error("Delete error:", err);
+    res.status(500).json({ message: "Failed to delete song", error: err });
+  }
+});
 
 // requests that use an API usually start with /api
 app.use("/api", router)
